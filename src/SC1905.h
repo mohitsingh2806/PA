3ce4d2d6603fe1beb5448aeb16635a924f6cb203
mohitsingh2806 <62180367+mohitsingh2806@users.noreply.github.com>
2024-01-29 22:09:32 +0530

    sc1905

---
+++ src/SC1905.h
@@ -0,0 +1,25 @@
+#ifndef SC1905_H
+#define SC1905_H
+
+#define FW_VER_REG 0x003  //8 bit R
+#define FW_BUILD_MSB_REG 0x004  //8 bit R
+#define FW_BUILD_LSB_REG 0x00A  //8bit R
+
+#define STATUS_REG 0x005  // 8bit R
+#define ERROR_REG 0x006 //8bit R
+#define WARNING_REG 0x007 //8bit R
+#define OUTPUT_MODE_REG 0x008 //8bit RW
+#define FREQ_RANGE_REG 0x010 //8bit R
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
